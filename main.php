4f6605c08623b2029131735e1cbc73491f9ab692
vardanAr <hoveyanvardan20@gmail.com>
2016-08-15 12:51:28 +0400

    Add files via upload

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>jjjjjj</title>
+		<link rel="stylesheet" type="text/css" href="aPordz.css">
+		<script src="aPordz.js"></script>
+	</head>
+	<body>
+		<div class="aPordz">
+			<?php
+				echo "ays podz@ hajoxvac e";
+			?>
+		
+			<button onclick="aPordz()">js</button>
+		
+		</div>
+	</body>
+</html>
